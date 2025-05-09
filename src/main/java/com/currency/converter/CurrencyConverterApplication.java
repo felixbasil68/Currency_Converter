d65cb6dbdbe12dc32a5968de27e228e2acd4ebd9
felixbasil68 <felixbasil12@gmail.com>
2025-05-10 01:51:02 +0530

    implement test case

--- src/main/java/com/currency/converter/CurrencyConverterApplication.java
+++ src/main/java/com/currency/converter/CurrencyConverterApplication.java
@@ -2,6 +2,8 @@ package com.currency.converter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CurrencyConverterApplication {
@@ -9,5 +11,9 @@ public class CurrencyConverterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConverterApplication.class, args);
 	}
+	 @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
